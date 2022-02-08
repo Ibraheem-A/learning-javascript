b5b856d99ade04eda652799dc108992a8ae800a7
Ibraheem-A <43247583+Ibraheem-A@users.noreply.github.com>
2022-02-08 01:53:18 +0100

    changing properties of items client side

--- src/theDom.js
+++ src/theDom.js
@@ -15,4 +15,10 @@ ul.remove();
 // edit
 ul.firstElementChild.textContent = 'Hi!'
 ul.children[1].innerText = 'John Doe'
-ul.lastElementChild.innerText = 'Bye!'
+ul.lastElementChild.innerHTML = '<h4>Bye!</h4>'
+
+// change button properties on cleint side
+
+const btn = document.querySelector('.btn');
+btn.style.color = 'white';
+btn.style.background = 'green';
